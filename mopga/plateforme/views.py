aa928203b0ced7920de7ebeb88fd7f2a0fc38d5b
Mathis <mathisdesnoes@gmail.com>
2020-12-09 19:27:06 +0100

    implementation de l'inscription

--- mopga/plateforme/views.py
+++ mopga/plateforme/views.py
@@ -4,10 +4,10 @@ from .models import Projet
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'plateforme/home/home.html')
 
 def projets(request):
     context = {
         'projets': Projet.objects.all()
     }
-    return render(request, 'projet/liste_projet.html', context)
+    return render(request, 'plateforme/projet/liste_projet.html', context)
