680cb004ca3a4faac1fdc18b9c93703abbf1fef6
Mathis <mathisdesnoes@gmail.com>
2020-12-11 11:35:52 +0100

    modification du profil

--- mopga/users/forms.py
+++ mopga/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +10,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','first_name','last_name','password1','password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username','email','first_name','last_name']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar']
--- mopga/users/models.py
+++ mopga/users/models.py
@@ -8,3 +8,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return "{} Profile".format(self.user.username)
+
+    # Remplace la méthode par défaut de model.save()
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.avatar.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.avatar.path)
+
